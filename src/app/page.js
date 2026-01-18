458a3abab48014dc56c3136a612184d26b3e39b2
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2026-01-18 06:26:16 -0500

    to do

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,60 @@
 "use client";
 import { useState } from "react";
 
-export default function Profile() {
-  const [user, setUser] = useState({
-    name: "John",
-    age: 20,
-    city: "Ulaanbaatar",
-  });
-
-  const changeName = () => {
-    setUser({
-      ...user,
-      name: "Mike",
-    });
+export default function NameManager() {
+  const [names, setNames] = useState([]);
+  const [input, setInput] = useState("");
+  const addName = () => {
+    if (input.trim() === "") return;
+    setNames([...names, input]);
+    setInput("");
+  };
+  const deleteName = (index) => {
+    setNames(names.filter((_, i) => i !== index));
   };
 
-  return (
-    <div className="flex flex-col items-center mt-20 gap-4">
-      <p>Name: {user.name}</p>
-      <p>Age: {user.age}</p>
-      <p>City: {user.city}</p>
 
-      <button onClick={changeName}>Change Name</button>
+
+  return (
+    <div>
+      <h2>Name List</h2>
+      <NameInput
+        input={input}
+        setInput={setInput}
+        addName={addName}
+      />
+      <NameList names={names} deleteName={deleteName} />
     </div>
   );
 }
+
+
+const NameInput=({ input, setInput, addName })=>{
+  return (
+    <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter name"
+      />
+      <button onClick={addName}>Add</button>
+    </div>
+  );
+}
+function NameList({ names, deleteName }) {
+  if (names.length === 0) {
+    return <p>No names yet</p>;
+  }
+
+  return (
+    <ul>
+      {names.map((name, index) => (
+        <li key={index}>
+          {name}
+          <button onClick={() => deleteName(index)}>❌</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
